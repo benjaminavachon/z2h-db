f5605df8d510af1d6645088fe30a62c6ee29d2bc
Benjamin Vachon <benjaminavachon@gmail.com>
2025-12-06 17:08:28 -0500

    changed create_db_header signature

--- include/parse.h
+++ include/parse.h
@@ -32,7 +32,7 @@ struct employee_t {
 
 
 
-int create_db_header(int fd, struct dbheader_t **headerOut);
+int create_db_header(struct dbheader_t **headerOut);
 
 int validate_db_header(int fd, struct dbheader_t **headerOut);
 
