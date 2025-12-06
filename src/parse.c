de26f9cb02e00def1b30334683ebe67ac644cb2e
Benjamin Vachon <benjaminavachon@gmail.com>
2025-12-06 16:40:14 -0500

    removed unused functions

--- src/parse.c
+++ src/parse.c
@@ -10,18 +10,6 @@
 #include "common.h"
 #include "parse.h"
 
-void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
-
-}
-
-int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
-
-}
-
-int read_employees(int fd, struct dbheader_t *dbhdr, struct employee_t **employeesOut) {
-
-}
-
 void output_file(int fd, struct dbheader_t *dbhdr) {
     if(fd < 0)
     {
