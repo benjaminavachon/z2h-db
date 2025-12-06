07ea4f0875be313953f81b229045c4ab3cac060f
Benjamin Vachon <benjaminavachon@gmail.com>
2025-12-06 18:46:26 -0500

    fixed hopefully

--- src/main.c
+++ src/main.c
@@ -98,11 +98,9 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    if (addstring) {
-		dbhdr->count++;
-		employees = realloc(employees, dbhdr->count*(sizeof(struct employee_t)));
-		add_employee(dbhdr, employees, addstring);
-	}
+    if(addstring) {
+        add_employee(dbhdr, &employees, addstring);
+    }
 
     if(list)
     {
